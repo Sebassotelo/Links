c1ec3bd8cbfefe4bb33d356e8d690394b64149c2
Sebas Sotelo <sebassotelo97@gmail.com>
2022-11-21 04:12:05 -0300

    ordenar con arrastre array

--- componentes/Layout.jsx
+++ componentes/Layout.jsx
@@ -12,16 +12,6 @@ function Layout({ children, title }) {
         <title>Links {title && `| ${title} `}</title>
         <meta name="descripcion" content="Generador de Links" />
         <link rel="icon" href="/favicon.ico" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap"
-          rel="stylesheet"
-        />
       </Head>
 
       {context.user && <Navbar />}
